8071d95d6958e10ede1ad8b013df58009d5c6c47
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-06-03 03:44:43 +0200

    awk: fix the evaluate bug related to warning fix
    Change-Id: I0682468b95fb870d519ac975c94dc5a8f70bdec0

--- editors/awk.c
+++ editors/awk.c
@@ -2410,11 +2410,11 @@ static var *evaluate(node *op, var *res)
 	v1 = nvalloc(2);
 
 	while (op) {
-		static struct {
+		struct {
 			var *v;
 			const char *s;
 		} L;
-		static struct {
+		struct {
 			var *v;
 			const char *s;
 		} R;
@@ -2914,7 +2914,7 @@ static var *evaluate(node *op, var *res)
 		}
 
 		case XC( OC_COMPARE ): {
-			static int i;
+			int i = 0;
 			double Ld;
 
 			if (is_numeric(L.v) && is_numeric(R.v)) {
