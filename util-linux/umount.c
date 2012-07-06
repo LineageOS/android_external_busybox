3dbe3d03bfd21c13b70f78adbf1c76f43e5f2f65
Denis Vlasenko <vda.linux@googlemail.com>
2012-07-06 13:41:53 -0500

    umount: instead of non-standard -D, use -d with opposite meaning
    This has been in upstream busybox for quite sometime, 2008-02-14 12:00:21 (GMT)
    And has no negative effects on android devices.
    Please see original commit:
    http://git.busybox.net/busybox/commit/util-linux/umount.c?id=b2e578a1f2c3cf317b391a7d2c059d6a5f5368b8&ss=1
    
    Change-Id: I7f8b9a6a88571b8190ffeaec75973f4ddf4f849e

--- util-linux/umount.c
+++ util-linux/umount.c
@@ -52,14 +52,13 @@ static struct mntent *getmntent_r(FILE* stream, struct mntent* result,
  * thus, on many systems, bare umount _does_ drop loop devices.
  * (2) many users request this feature.
  */
-#define OPTION_STRING           "fldDnra" "vt:i"
+#define OPTION_STRING           "fldnra" "vt:i"
 #define OPT_FORCE               (1 << 0) // Same as MNT_FORCE
 #define OPT_LAZY                (1 << 1) // Same as MNT_DETACH
-//#define OPT_FREE_LOOP           (1 << 2) // -d is assumed always present
-#define OPT_DONT_FREE_LOOP      (1 << 3)
-#define OPT_NO_MTAB             (1 << 4)
-#define OPT_REMOUNT             (1 << 5)
-#define OPT_ALL                 (ENABLE_FEATURE_UMOUNT_ALL ? (1 << 6) : 0)
+#define OPT_FREELOOP            (1 << 2) 
+#define OPT_NO_MTAB             (1 << 3)
+#define OPT_REMOUNT             (1 << 4)
+#define OPT_ALL                 (ENABLE_FEATURE_UMOUNT_ALL ? (1 << 5) : 0)
 
 int umount_main(int argc, char **argv) MAIN_EXTERNALLY_VISIBLE;
 int umount_main(int argc UNUSED_PARAM, char **argv)
@@ -116,7 +115,7 @@ int umount_main(int argc UNUSED_PARAM, char **argv)
 		if (!argv[0])
 			bb_show_usage();
 		m = NULL;
-	}
+	}http://review.cyanogenmod.com/#/c/18561/3/util-linux/umount.c
 
 	// Loop through everything we're supposed to umount, and do so.
 	for (;;) {
@@ -173,7 +172,7 @@ int umount_main(int argc UNUSED_PARAM, char **argv)
 		} else {
 			// De-allocate the loop device.  This ioctl should be ignored on
 			// any non-loop block devices.
-			if (ENABLE_FEATURE_MOUNT_LOOP && !(opt & OPT_DONT_FREE_LOOP) && m)
+			if (ENABLE_FEATURE_MOUNT_LOOP && (opt & OPT_FREELOOP) && m)
 				del_loop(m->device);
 			if (ENABLE_FEATURE_MTAB_SUPPORT && !(opt & OPT_NO_MTAB) && m)
 				erase_mtab(m->dir);
