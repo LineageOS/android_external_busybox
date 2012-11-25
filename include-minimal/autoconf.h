9168093f74bc314c003c5f99e2a9c7a62ca087b7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-25 17:46:35 +0100

    update config
    Change-Id: I356dff95eb8f558cda9f3280864f8e5c1ccdb972

--- include-minimal/autoconf.h
+++ include-minimal/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.20.2-jb
  */
-#define AUTOCONF_TIMESTAMP "2012-10-25 21:29 +0100"
+#define AUTOCONF_TIMESTAMP "2012-11-25 17:47 +0100"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
