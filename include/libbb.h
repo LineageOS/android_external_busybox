bfc6e1a8b32d087133e492c82b7e11ee8ea1cd86
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-21 18:43:54 +0530

    enable SELinux [WIP]
    require external/libsepol static lib patch
    
    note: Work In Progress but doesnt break the build
          Only use this for tests
    
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>
    
    Change-Id: I4076da694a6192b44b3f143c095e8990a9a6efa9

--- include/libbb.h
+++ include/libbb.h
@@ -94,8 +94,13 @@
 #if ENABLE_SELINUX
 # include <selinux/selinux.h>
 # include <selinux/context.h>
+#if defined(ANDROID) || defined(__ANDROID__)
+# include "android_selinux.h"
+# include <sepol/policydb/flask.h>
+#else
 # include <selinux/flask.h>
 # include <selinux/av_permissions.h>
+#endif
 #endif
 #if ENABLE_FEATURE_UTMP
 # include <utmp.h>
