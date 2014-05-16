c7bcbf91ebcd72658d2b60299a98eef48d50a609
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-05-16 20:01:11 +0200

    selinux: enable support for bionic
    require libsepol package (mm in external/libsepol or add package)
    
    Tested applets:
      getenforce, setenforce, sestatus, ls -Z, getsebool, setsebool
      runcon, selinuxenabled, matchpathcon, chcon, restorecon, setfiles
    
    Notes:
      matchpathcon selabel_lookup was broken by the incompatible regexec
      which is now in bionic libc (netbsd part), libselinux linkage use
      busybox one, so a rename of internal regex functions was required
    
      recovery will not use bb_regex lib, so regex commands could not
      work exactly like xbin or static ones (sed/less/expr/awk/pgrep)
    
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>
    
    Change-Id: Id82b8a7672a669e3f95360bb70b7b127591f2080

--- archival/libarchive/data_extract_all.c
+++ archival/libarchive/data_extract_all.c
@@ -14,9 +14,13 @@ void FAST_FUNC data_extract_all(archive_handle_t *archive_handle)
 
 #if ENABLE_FEATURE_TAR_SELINUX
 	char *sctx = archive_handle->tar__sctx[PAX_NEXT_FILE];
+#ifdef __BIONIC__
+	matchpathcon_init(NULL);
+#endif
 	if (!sctx)
 		sctx = archive_handle->tar__sctx[PAX_GLOBAL];
 	if (sctx) { /* setfscreatecon is 4 syscalls, avoid if possible */
+		bb_info_msg(" setfscreatecon(%s)", sctx);
 		setfscreatecon(sctx);
 		free(archive_handle->tar__sctx[PAX_NEXT_FILE]);
 		archive_handle->tar__sctx[PAX_NEXT_FILE] = NULL;
