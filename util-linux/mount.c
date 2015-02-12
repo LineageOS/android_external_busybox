bbfc3392b8e61dd4c63b1926bab20445ac245bd3
Dan Pasanen <dan.pasanen@gmail.com>
2015-02-11 20:35:23 -0600

    mount: respect a user-specified fstype
    * Don't attempt to auto-detect the filesystem type using blkid
      if the user has specifically specified a type
    
    Change-Id: Icebbc3189fc59af9fe52e12b693f3b7ae516e2c7

--- util-linux/mount.c
+++ util-linux/mount.c
@@ -341,6 +341,7 @@ struct globals {
 	unsigned verbose;
 #endif
 	llist_t *fslist;
+	int user_fstype;
 	char getmntent_buf[1];
 } FIX_ALIASING;
 enum { GETMNTENT_BUFSIZE = COMMON_BUFSIZE - offsetof(struct globals, getmntent_buf) };
@@ -352,6 +353,7 @@ enum { GETMNTENT_BUFSIZE = COMMON_BUFSIZE - offsetof(struct globals, getmntent_b
 #define verbose           0
 #endif
 #define fslist            (G.fslist           )
+#define user_fstype       (G.user_fstype      )
 #define getmntent_buf     (G.getmntent_buf    )
 #define INIT_G() do { } while (0)
 
@@ -1784,13 +1786,20 @@ static int singlemount(struct mntent *mp, int ignore_busy)
 
 	vfsflags = parse_mount_options(mp->mnt_opts, &filteropts);
 
-	detected_fstype = get_fstype_from_devname(mp->mnt_fsname);
+	if (user_fstype) {
+		// Treat fstype "auto" as unspecified
+		if (mp->mnt_type && !strcmp(mp->mnt_type, "auto"))
+			mp->mnt_type = NULL;
+	} else {
+		// If user didn't specify an fstype and blkid disagrees or the
+		// fstype is "auto", trust blkid's determination of the fstype.
 
-	// If fstype is auto or disagrees with blkid, trust blkid's
-	// determination of the filesystem type
-	if ((mp->mnt_type && !strcmp(mp->mnt_type, "auto")) ||
-	    (detected_fstype && strcmp(detected_fstype, mp->mnt_type)))
-		mp->mnt_type = detected_fstype;
+		detected_fstype = get_fstype_from_devname(mp->mnt_fsname);
+
+		if ((mp->mnt_type && !strcmp(mp->mnt_type, "auto")) ||
+		    (detected_fstype && strcmp(detected_fstype, mp->mnt_type)))
+			mp->mnt_type = detected_fstype;
+	}
 
 	// Might this be a virtual filesystem?
 	if (ENABLE_FEATURE_MOUNT_HELPERS && strchr(mp->mnt_fsname, '#')) {
@@ -2067,6 +2076,8 @@ int mount_main(int argc UNUSED_PARAM, char **argv)
 	opt_complementary = "?2o::" IF_FEATURE_MOUNT_VERBOSE("vv");
 	opt = getopt32(argv, OPTION_STR, &lst_o, &fstype, &O_optmatch
 			IF_FEATURE_MOUNT_VERBOSE(, &verbose));
+
+	if (opt & OPT_t) user_fstype = 1;
 	while (lst_o) append_mount_options(&cmdopts, llist_pop(&lst_o)); // -o
 	if (opt & OPT_r) append_mount_options(&cmdopts, "ro"); // -r
 	if (opt & OPT_w) append_mount_options(&cmdopts, "rw"); // -w
@@ -2280,6 +2291,8 @@ int mount_main(int argc UNUSED_PARAM, char **argv)
 			mtcur->mnt_opts = xstrdup(mtcur->mnt_opts);
 			append_mount_options(&(mtcur->mnt_opts), cmdopts);
 			resolve_mount_spec(&mtpair->mnt_fsname);
+			if (user_fstype)
+				mtcur->mnt_type = fstype;
 			rc = singlemount(mtcur, /*ignore_busy:*/ 0);
 			if (ENABLE_FEATURE_CLEAN_UP)
 				free(mtcur->mnt_opts);
