ec135f1ff6a435ef45e3c83f1060974fcc28a8da
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-05 16:08:44 -0600

    mount: address a couple crashes
    Change-Id: I3f978f40eb77d7679decf4a33b5c655ebb74d979

--- util-linux/mount.c
+++ util-linux/mount.c
@@ -1796,7 +1796,7 @@ static int singlemount(struct mntent *mp, int ignore_busy)
 
 		detected_fstype = get_fstype_from_devname(mp->mnt_fsname);
 
-		if ((mp->mnt_type && !strcmp(mp->mnt_type, "auto")) ||
+		if (mp->mnt_type && (!strcmp(mp->mnt_type, "auto")) ||
 		    (detected_fstype && strcmp(detected_fstype, mp->mnt_type)))
 			mp->mnt_type = detected_fstype;
 	}
