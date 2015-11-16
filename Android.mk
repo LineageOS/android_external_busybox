0f9b71feceb247ab76698f403b238797a3ee9e5b
Steve Kondik <steve@cyngn.com>
2015-11-16 11:05:42 -0800

    Disable link generation for Busybox
     * In preparation for switching to Toybox, stop generating links
       in /system/xbin for Busybox.
    
    Change-Id: Idcf0349b2eb7e1612bf3a40bd91c3df9690f13a0

--- Android.mk
+++ Android.mk
@@ -157,6 +157,8 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(busybox_prepare_full)
 LOCAL_CLANG := false
 include $(BUILD_EXECUTABLE)
 
+ifeq ($(WITH_BUSYBOX_LINKS),true)
+
 BUSYBOX_LINKS := $(shell cat $(BB_PATH)/busybox-$(BUSYBOX_CONFIG).links)
 # nc is provided by external/netcat
 exclude := nc
@@ -175,6 +177,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
+endif
 
 # Static Busybox
 
