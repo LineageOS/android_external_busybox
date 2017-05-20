d017eb03306f5085a8c7e5b6729e653ab6134f00
sayeed99 <sayeed.afridi2009@gmail.com>
2017-05-20 17:44:41 +0530

    Busybox : BIONIC_N has its own strchrnul, use it and fix compilation.
    Change-Id: Id26c3dd39f7153872fa83a84ff4a40ece51242bf

--- include/platform.h
+++ include/platform.h
@@ -454,12 +454,14 @@ typedef unsigned smalluint;
 
 #if defined(ANDROID) || defined(__ANDROID__)
 # undef HAVE_DPRINTF
-# undef HAVE_STRCHRNUL
 # undef HAVE_STRVERSCMP
 # undef HAVE_UNLOCKED_LINE_OPS
 # undef HAVE_NET_ETHERNET_H
 # ifndef BIONIC_L
 #  undef HAVE_STPCPY
+# endif
+# ifndef BIONIC_N
+#  undef HAVE_STRCHRNUL
 # endif
 #endif
 
